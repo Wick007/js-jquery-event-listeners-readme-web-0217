1d918bd15786064730bf35fefd06c1999841494c
Wick007 <a.wick17@gmail.com>
2017-04-03 10:35:37 -0400

    Done.

--- js/events.js
+++ js/events.js
@@ -1,5 +1,37 @@
 //define functions here
 
+var getIt = function() {
+  $('p').on('click', function(){
+    alert('hey!')
+  })
+}
+
+var frameIt = function(){
+  $('img').on('load', function(){
+    $('img').addClass('tasty')
+  })
+}
+
+var pressIt = function(){
+  $('#typing').on('keydown', function(e){
+    if (e.which === 71) {
+      alert("You pressed 'g'!")
+    }
+  })
+}
+
+var submitIt = function() {
+  $('form').on('submit', function(e) {
+    alert("Your form is going to be submitted now.")
+  })
+}
+
+// function submitIt() {
+//   $('form').on('submit', function(e) {
+//     alert('Your form is going to be submitted now.')
+//   })
+// }
+
 $(document).ready(function(){
 
 // call functions here
